984faba217737832ef62fa83142f3556bc9abc00
rk <raymondkalonji@gmail.com>
2020-11-13 06:54:27 -0500

    feat: adding script generating heartbeats

---
+++ backend/generate_heartbeats.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Until user kills program generate and submit a heartbeat for a random sensor
+while [ 1 != 2 ]
+do
+    sleep 1
+    curl -X POST "http://localhost:5000/api/v0/heartbeat?sensor_id=$((1 + $RANDOM % 10))&timestamp=$(date +%s)"
+done
