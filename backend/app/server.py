9df46d9797294dad10e947160eb711242b02deb0
rk <raymondkalonji@gmail.com>
2020-11-13 06:59:38 -0500

    feat: persising heartbeats to db

--- backend/app/server.py
+++ backend/app/server.py
@@ -1,14 +1,34 @@
-from flask import Flask
+import sys
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app.models import User, Sensor, Heartbeat
+from app.models import Heartbeat
+from datetime import datetime
 
 from config import Config
-
 from app import app
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 @app.route('/api/v0/heartbeat', methods=['POST'])
 def post_heartbit():
-    print('posting heartbeat')
-    return ''
+    print(request.args)
+    return_value = "success"
+    try:
+        heartbeat = Heartbeat(sensor_id=int(request.args.get('sensor_id')),
+                            timestamp=datetime.fromtimestamp(int(request.args.get('timestamp'))))
+        db.session.add(heartbeat)
+        db.session.commit()
+    except IOError:
+        print(sys.exc_info()[0])
+        return_value = "IO Error"
+    except ValueError:
+        print(sys.exc_info()[0])
+        return_value = "value error"
+    except:
+        print(sys.exc_info()[0])
+        return_value = "unhandled error"
+    
+    return return_value
 
